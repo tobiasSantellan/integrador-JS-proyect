698ca0e974afa11524bb6bc4bdde3ae6a27331d7
tobiasSantellan <tobias.ariel.santellan@gmail.com>
2023-08-29 00:49:36 -0300

    "push"

---
+++ carrito.js
@@ -0,0 +1,108 @@
+let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+productosEnCarrito = JSON.parse(productosEnCarrito);
+
+const d = document;
+
+const carritoVacio = d.querySelector("#carrito-vacio");
+const carritoProductos = d.querySelector("#carrito-productos");
+const carritoAcciones = d.querySelector("#carrito-acciones");
+const carritoComprado = d.querySelector("#carrito-comprado");
+let botonesEliminar = d.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = d.querySelector("#carrito-acciones-vaciar");
+const total = d.querySelector("#total");
+const botonComprar = d.querySelector("#carrito-acciones-comprar");
+
+function cargarProductosCarrito () {
+
+    if(productosEnCarrito && productosEnCarrito.length > 0) {
+
+        carritoVacio.classList.add("disabled");
+        carritoProductos.classList.remove("disabled");
+        carritoAcciones.classList.remove("disabled");
+        carritoComprado.classList.add("disabled");
+    
+        carritoProductos.innerHTML = "";
+    
+        productosEnCarrito.forEach(producto => {
+            const div = d.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+            <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="carrito-producto-titulo">
+                <small>Título</small>
+                <h3>${producto.titulo}</h3>
+            </div>
+            <div class="carrito-producto-cantidad">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="carrito-producto-precio">
+                <small>Precio</small>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="carrito-producto-subtotal">
+                <small>Subtotal</small>
+                <p>${producto.precio * producto.cantidad}</p>
+            </div>
+            <button class="carrito-producto-eliminar" id="${producto.id}"><i class="fa-solid fa-trash-can"></i></button>
+            `;
+    
+            carritoProductos.append(div);
+        })
+
+    } else {
+        carritoVacio.classList.remove("disabled");
+        carritoProductos.classList.add("disabled");
+        carritoAcciones.classList.add("disabled");
+        carritoComprado.classList.add("disabled");
+    }
+
+    actualizarBotonesEliminar();
+    actualizarTotal();
+}
+
+cargarProductosCarrito();
+
+function actualizarBotonesEliminar () {
+    botonesEliminar = d.querySelectorAll(".carrito-producto-eliminar");
+
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito);
+    })
+}
+
+function eliminarDelCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    // console.log(idBoton)
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    productosEnCarrito.splice(index, 1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+}
+
+
+botonVaciar.addEventListener("click", vaciarCarrito)
+function vaciarCarrito () {
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    cargarProductosCarrito();
+}
+
+function actualizarTotal () {
+    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
+    total.innerText = `$${totalCalculado}`;
+}
+
+botonComprar.addEventListener("click", comprarCarrito)
+function comprarCarrito () {
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+        carritoVacio.classList.add("disabled");
+        carritoProductos.classList.add("disabled");
+        carritoAcciones.classList.add("disabled");
+        carritoComprado.classList.remove("disabled");
+}
--- main.js
+++ main.js
@@ -3,259 +3,346 @@ const productos = [
     // buzos 
     {
         id: "buzo-01",
-        titulo: "Buzo Blacki",
+        titulo: "Blacki",
         imagen: "assets/buzo-1.jpg",
         categoria: {
             nombre: "Buzos",
             id: "buzos"
         },
-        precio: 1000
+        precio: 15000
     },
     {
         id: "buzo-02",
-        titulo: "Buzo Grey",
+        titulo: "Grey",
         imagen: "assets/buzo-2.jpg",
         categoria: {
             nombre: "Buzos",
             id: "buzos"
         },
-        precio: 1000
+        precio: 20000
     },
     {
         id: "buzo-03",
-        titulo: "Buzo September",
+        titulo: "June",
         imagen: "assets/buzo-3.jpg",
         categoria: {
             nombre: "Buzos",
             id: "buzos"
         },
-        precio: 1000
+        precio: 22000
     },
     {
         id: "buzo-04",
-        titulo: "Buzo Ghost",
+        titulo: "Ghost",
         imagen: "assets/buzo-4.jpg",
         categoria: {
             nombre: "Buzos",
             id: "buzos"
         },
-        precio: 1000
+        precio: 30000
     },
     {
         id: "buzo-05",
-        titulo: "Buzo Butter",
+        titulo: "Butter",
         imagen: "assets/buzo-5.jpg",
         categoria: {
             nombre: "Buzos",
             id: "buzos"
         },
-        precio: 1000
+        precio: 25000
     },
     {
         id: "buzo-06",
-        titulo: "Buzo Broken",
+        titulo: "Broken",
         imagen: "assets/buzo-img.jpg",
         categoria: {
             nombre: "Buzos",
             id: "buzos"
         },
-        precio: 1000
+        precio: 35000
     },
     // camperas 
     {
         id: "campera-01",
-        titulo: "Campera Kanyie",
+        titulo: "Kanye",
         imagen: "assets/campera-1.jpg",
         categoria: {
             nombre: "Camperas",
             id: "camperas"
         },
-        precio: 1000
+        precio: 40000
     },
     {
         id: "campera-02",
-        titulo: "Campera Brown",
+        titulo: "Brown",
         imagen: "assets/campera-2.jpg",
         categoria: {
             nombre: "Camperas",
             id: "camperas"
         },
-        precio: 1000
+        precio: 42000
     },
     {
         id: "campera-03",
-        titulo: "Campera Mountain",
+        titulo: "Mountain",
         imagen: "assets/campera-3.jpg",
         categoria: {
             nombre: "Camperas",
             id: "camperas"
         },
-        precio: 1000
+        precio: 44000
     },
     {
         id: "campera-04",
-        titulo: "Campera Future",
+        titulo: "Future",
         imagen: "assets/campera-4.jpg",
         categoria: {
             nombre: "Camperas",
             id: "camperas"
         },
-        precio: 1000
+        precio: 60000
     },
     {
         id: "campera-05",
-        titulo: "Campera Air",
+        titulo: "Air",
         imagen: "assets/campera-5.jpg",
         categoria: {
             nombre: "Camperas",
             id: "camperas"
         },
-        precio: 1000
+        precio: 39000
     },
     {
         id: "campera-06",
-        titulo: "Campera Bomber",
+        titulo: "Bomber",
         imagen: "assets/bomber-img.jpg",
         categoria: {
             nombre: "Camperas",
             id: "camperas"
         },
-        precio: 1000
+        precio: 55000
     },
     // pantalones
     {
         id: "pantalon-01",
-        titulo: "Jean Sky",
+        titulo: "Sky",
         imagen: "assets/jean-1.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 30000
     },
     {
         id: "pantalon-02",
-        titulo: "Jean Solid",
+        titulo: "Solid",
         imagen: "assets/jean-2.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 29000
     },
     {
         id: "pantalon-03",
-        titulo: "Jean Bread",
+        titulo: "Bread",
         imagen: "assets/jean-3.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 35000
     },
     {
         id: "pantalon-04",
-        titulo: "Jean Star",
+        titulo: "Star",
         imagen: "assets/jean-4.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 28000
     },
     {
         id: "pantalon-05",
-        titulo: "Jean White",
+        titulo: "White",
         imagen: "assets/jean-5.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 33000
     },
     {
         id: "pantalon-06",
-        titulo: "Jean Faded",
+        titulo: "Faded",
         imagen: "assets/jean-img.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 37000
     },
     // remeras
     {
         id: "remera-01",
-        titulo: "Remera Choise",
+        titulo: "Chois",
         imagen: "assets/remera-1.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remeras"
         },
-        precio: 1000
+        precio: 14000
     },
     {
         id: "remera-02",
-        titulo: "Remera Happen",
+        titulo: "Happen",
         imagen: "assets/remera-2.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remeras"
         },
-        precio: 1000
+        precio: 13000
     },
     {
         id: "remera-03",
-        titulo: "Remera Basic",
+        titulo: "Basic",
         imagen: "assets/remera-3.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remeras"
         },
-        precio: 1000
+        precio: 11000
     },
     {
         id: "remera-04",
-        titulo: "Remera Crud",
+        titulo: "Crud",
         imagen: "assets/remera-4.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remeras"
         },
-        precio: 1000
+        precio: 16000
     },
     {
         id: "remera-05",
-        titulo: "Remera LA",
+        titulo: "Acid",
         imagen: "assets/remera-5.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remeras"
         },
-        precio: 1000
+        precio: 12000
     },
     {
         id: "remera-06",
-        titulo: "Remera Vision",
+        titulo: "Vision",
         imagen: "assets/remera-6.jpg",
         categoria: {
             nombre: "Remeras",
             id: "remeras"
         },
-        precio: 1000
+        precio: 19000
     },
 ]
 
-
 const d = document;
 const header = d.querySelector(".header");
+const contenedorProductos = d.querySelector("#contenedor-productos");
+const botonesCategorias = d.querySelectorAll(".boton-categoria");
+let botonesAgregar = d.querySelectorAll(".producto-agregar");
+const numerito = d.querySelector("#numerito");
 
-const headerScroll = () => {
-     (window.scrollY > 0) ? header.style.backgroundColor = "#090909" : header.style.backgroundColor = "transparent"
-    }
 
-const init = () => {
-    window.addEventListener("scroll", headerScroll)
+const headerScroll = () => (window.scrollY > 0) ? header.style.backgroundColor = "#090909" : header.style.backgroundColor = "transparent"
+    
+window.addEventListener("scroll", headerScroll);
+
+
+function cargarProductos (productosElegidos)  {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = d.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <p class="producto-precio">$${producto.precio}</p>
+            <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>
+     `;
+
+     contenedorProductos.append(div)
+    })
+
+    actualizarBotonesAgregar();
 }
 
-init();
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+
+        e.currentTarget.classList.add("active");
+
+        if(e.currentTarget.id != "todos") {
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            cargarProductos(productos);
+        }
+    })
+})
+
+function actualizarBotonesAgregar () {
+    botonesAgregar = d.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    })
+}
+
+let productosEnCarrito;
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if(productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumeroEnCarrito();
+} else {
+    productosEnCarrito = [];
+}
+
+
+function agregarAlCarrito (e)  {
+    
+    const idBoton = e.currentTarget.id;
+    // console.log(idBoton)
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    // console.log(productoAgregado)
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+       const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    // console.log(index)
+       productosEnCarrito[index].cantidad++;
+
+    } else {
+        productoAgregado.cantidad = 1; // propiedad nueva a mi array productos
+        productosEnCarrito.push(productoAgregado)
+    }
+    // console.log(productosEnCarrito)
+    actualizarNumeroEnCarrito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumeroEnCarrito () {
+    let nuevoNumero = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    // console.log(nuevoNumero)
+    numerito.innerHTML = nuevoNumero;
+}
+
